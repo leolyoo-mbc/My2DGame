488eccb4a7e2453fd21bf9e2d68a4ffaacb4299d
leolyoo-mbc <leolyoo.mbc@gmail.com>
2026-06-18 18:56:33 +0900

    Add 2D art, tilesets, animations and samples
    Import a large set of 2D art and tilemap resources: player animations/controllers, many player sprite metadata, a Tileset with numerous tile assets, rule tiles, and a RectangularPalette prefab. Also add a Sample TileMapTest with sprites and prefabs. Update SampleScene and tweak ParallaxEffect.cs and PlayerController.cs to integrate the new assets.

--- Assets/_My2DGame/Scripts/ParallaxEffect.cs
+++ Assets/_My2DGame/Scripts/ParallaxEffect.cs
@@ -1,37 +1,26 @@
 using UnityEngine;
 
-namespace My2DGame
+public class ParallaxEffect : MonoBehaviour
 {
-    public class ParallaxEffect : MonoBehaviour
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    private void Start()
     {
-        [SerializeField] private float speedModifier = 1.0f;
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
 
-        private Transform cameraTransform;
-        private Vector3 lastCameraPosition;
-        private float initialZ;
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
 
-        private void Start()
-        {
-            cameraTransform = Camera.main.transform;
-            lastCameraPosition = cameraTransform.position;
-            initialZ = transform.position.z;
-        }
+        // [핵심] Z가 0이면 0(정지), Z가 20 이상이면 1(동기화)로 매핑
+        // Z값을 20으로 나누어 0~1 사이의 비율로 만듭니다.
+        float parallaxMultiplier = Mathf.Clamp01(transform.position.z / 20f);
 
-        private void LateUpdate()
-        {
-            // 1. 카메라가 이동한 거리 계산
-            Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxMultiplier, 0, 0);
 
-            // 2. Z축 거리에 따른 패럴랙스 강도 계산
-            // 카메라(z=0)에서 배경(initialZ)까지의 거리를 이용하여 이동 비례 결정
-            // (뒤에 있을수록 0에 가깝게, 앞에 있을수록 1에 가깝게)
-            float parallaxFactor = Mathf.Abs(initialZ) / 10f; // 10은 기준 거리(조절 가능)
-
-            // 3. 이동 적용
-            // 카메라가 이동한 만큼 배경도 함께 이동하되, Z축 비례만큼 줄여줌
-            transform.position += new Vector3(deltaMovement.x * (1f - parallaxFactor), 0, 0);
-
-            lastCameraPosition = cameraTransform.position;
-        }
+        lastCameraPosition = cameraTransform.position;
     }
 }
--- Assets/_My2DGame/Scripts/PlayerController.cs
+++ Assets/_My2DGame/Scripts/PlayerController.cs
@@ -3,21 +3,35 @@ using UnityEngine.InputSystem;
 
 namespace My2DGame
 {
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Rigidbody2D))]
     /// <summary>
     /// 플레이어 캐릭터의 움직임과 행동을 제어하는 클래스
     /// </summary>
-    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 5f;
+        #region Variables
+        [SerializeField] private float walkSpeed = 5f;
+        [SerializeField] private float runSpeed = 10f;
+
         private Rigidbody2D rb;
         private Vector2 moveInput;
+        private bool isMove = false;
+        private bool isRun = false;
 
+        private Animator animator;
+        #endregion
+
+
+        #region Unity Event Method
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
         }
+        #endregion
 
+        #region Custom Method
         // New Input System의 "Move" 액션이 연결된 이벤트 함수
         public void OnMove(InputAction.CallbackContext context)
         {
@@ -25,11 +39,42 @@ namespace My2DGame
             moveInput = context.ReadValue<Vector2>();
         }
 
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            if (context.performed) isRun = true;
+            else isRun = false;
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.started) Debug.Log("점프키를 started");
+            if (context.performed) Debug.Log("점프키를 performed");
+            if (context.canceled) Debug.Log("점프키를 canceled");
+        }
+
         private void FixedUpdate()
         {
             // 리지드바디의 속도를 입력받은 x값 * 속도로 설정
             // y값은 현재 속도를 유지하여 점프나 중력 등에 영향받지 않게 함
-            rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+            if (isRun)
+            {
+                Debug.Log("왼쪽 쉬프트키를 누르고 있습니다");
+                rb.linearVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
+            }
+            else rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
+
+            if (moveInput != Vector2.zero)
+            {
+                isMove = true;
+            }
+            else
+            {
+                isMove = false;
+            }
+
+            animator.SetBool("IsMove", isMove);
+            animator.SetBool("IsRun", isRun);
         }
+        #endregion
     }
 }
