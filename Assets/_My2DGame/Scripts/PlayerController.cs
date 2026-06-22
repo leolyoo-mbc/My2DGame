dbcc7c5ea28aa57cc5e6d0cc6d4efe5577661bf9
leolyoo-mbc <leolyoo.mbc@gmail.com>
2026-06-22 18:50:59 +0900

    Add PlayScene, animations & state machine
    Add a new PlayScene and supporting assets for the 2D game: many Player and Enemy animation clips and controllers, sprite/texture meta entries, and new folder meta files. Add SetBoolStateMachine script and related meta, and add various animation assets under _My2DGame/Arts. Update Player controller asset and modify PlayerController.cs. Adjust InputSystem_Actions.inputactions by clearing expectedControlType for Attack, Jump and Sprint. Also update ProjectSettings TagManager and solution file entries. These changes introduce the playable scene and core animation/state infrastructure.

--- Assets/_My2DGame/Scripts/PlayerController.cs
+++ Assets/_My2DGame/Scripts/PlayerController.cs
@@ -5,75 +5,161 @@ namespace My2DGame
 {
     [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Collider2D))]
     /// <summary>
     /// 플레이어 캐릭터의 움직임과 행동을 제어하는 클래스
     /// </summary>
     public class PlayerController : MonoBehaviour
     {
-        #region Variables
+        #region Variables   
+        private static readonly int CannotMoveHash = Animator.StringToHash("CannotMove");
+        private static readonly int AttackTriggerHash = Animator.StringToHash("AttackTrigger");
+        private static readonly int YVelocityHash = Animator.StringToHash("YVelocity");
+        private static readonly int IsGroundedHash = Animator.StringToHash("IsGrounded");
+        private static readonly int JumpTriggerHash = Animator.StringToHash("JumpTrigger");
+        private static readonly int IsMoveHash = Animator.StringToHash("IsMove");
+        private static readonly int IsRunHash = Animator.StringToHash("IsRun");
+
         [SerializeField] private float walkSpeed = 5f;
         [SerializeField] private float runSpeed = 10f;
+        [SerializeField] private float jumpForce = 10f;
 
         private Rigidbody2D rb;
         private Vector2 moveInput;
         private bool isMove = false;
         private bool isRun = false;
+        private bool isFacingRight = true;
+        private bool isGround = true;
 
         private Animator animator;
+        private Collider2D playerCollider;
+
+        [SerializeField] private ContactFilter2D contactFilter;
+        private RaycastHit2D[] results = new RaycastHit2D[1];
+        [SerializeField] private float groundDistance = 0.05f;
         #endregion
 
+        #region Property
+        public bool IsMove
+        {
+            get => isMove;
+            private set
+            {
+                isMove = value;
+                animator.SetBool(IsMoveHash, value);
+            }
+        }
+
+        public bool IsRun
+        {
+            get => isRun;
+            private set
+            {
+                isRun = value;
+                animator.SetBool(IsRunHash, value);
+            }
+        }
+
+        public bool IsFacingRight
+        {
+            get => isFacingRight;
+            private set
+            {
+                if (isFacingRight != value)
+                {
+                    isFacingRight = value;
+                    Vector3 currentScale = transform.localScale;
+                    currentScale.x *= -1;
+                    transform.localScale = currentScale;
+                }
+            }
+        }
+
+        public bool IsGround
+        {
+            get => isGround;
+            private set
+            {
+                isGround = value;
+                animator.SetBool(IsGroundedHash, value);
+            }
+        }
+        #endregion
 
         #region Unity Event Method
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
+            playerCollider = GetComponent<Collider2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            bool cannotMove = animator.GetBool(CannotMoveHash);
+
+            // 1. 이동 및 애니메이션 상태 처리
+            if (!cannotMove)
+            {
+                // 이동 속도 적용
+                float targetSpeed = isRun ? runSpeed : walkSpeed;
+                rb.linearVelocity = new Vector2(moveInput.x * targetSpeed, rb.linearVelocity.y);
+
+                // 방향 및 걷기 애니메이션 상태 처리
+                if (Mathf.Abs(moveInput.x) > 0.01f)
+                {
+                    IsMove = true;
+                    IsFacingRight = moveInput.x > 0;
+                }
+                else
+                {
+                    IsMove = false;
+                }
+            }
+            else
+            {
+                // 수정: X축 이동만 정지시키고 Y축(중력 및 현재 낙하 속도)은 유지합니다.
+                rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
+                IsMove = false; // 이동 불가 상태에서는 걷기 애니메이션도 강제로 정지
+            }
+
+            // 2. 바닥 감지 처리
+            IsGround = playerCollider.Cast(Vector2.down, contactFilter, results, groundDistance) > 0;
+
+            // 3. 추락/상승 애니메이션 처리
+            animator.SetFloat(YVelocityHash, rb.linearVelocity.y);
         }
         #endregion
 
         #region Custom Method
-        // New Input System의 "Move" 액션이 연결된 이벤트 함수
         public void OnMove(InputAction.CallbackContext context)
         {
-            // ReadValue를 통해 입력값(Vector2)을 가져옵니다.
             moveInput = context.ReadValue<Vector2>();
         }
 
         public void OnSprint(InputAction.CallbackContext context)
         {
-            if (context.performed) isRun = true;
-            else isRun = false;
+            if (context.started) IsRun = true;
+            else if (context.canceled) IsRun = false;
         }
 
         public void OnJump(InputAction.CallbackContext context)
         {
-            if (context.started) Debug.Log("점프키를 started");
-            if (context.performed) Debug.Log("점프키를 performed");
-            if (context.canceled) Debug.Log("점프키를 canceled");
+            // 수정: 이동 불가 상태가 아닐 때만 점프가 가능하도록 조건 추가
+            if (context.started && IsGround && !animator.GetBool(CannotMoveHash))
+            {
+                animator.SetTrigger(JumpTriggerHash);
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
         }
 
-        private void FixedUpdate()
+        public void OnAttack(InputAction.CallbackContext context)
         {
-            // 리지드바디의 속도를 입력받은 x값 * 속도로 설정
-            // y값은 현재 속도를 유지하여 점프나 중력 등에 영향받지 않게 함
-            if (isRun)
+            // 수정: 이동 불가 상태가 아닐 때만 공격이 가능하도록 조건 추가 (연속 공격 등 예외 상황 제외)
+            if (context.started && IsGround && !animator.GetBool(CannotMoveHash))
             {
-                Debug.Log("왼쪽 쉬프트키를 누르고 있습니다");
-                rb.linearVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
+                animator.SetTrigger(AttackTriggerHash);
             }
-            else rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
-
-            if (moveInput != Vector2.zero)
-            {
-                isMove = true;
-            }
-            else
-            {
-                isMove = false;
-            }
-
-            animator.SetBool("IsMove", isMove);
-            animator.SetBool("IsRun", isRun);
         }
         #endregion
     }
---
+++ Assets/_My2DGame/Scripts/StateMachine/SetBoolStateMachine.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace My2DGame
+{
+    public class SetBoolStateMachine : StateMachineBehaviour
+    {
+        #region Variables
+        //bool형 파라미터 이름
+        public string boolName;
+
+        //상태 머신 들어올 때의 bool형 값
+        public bool enterValue;
+
+        //상태 머신 나갈 때의 bool형 값
+        public bool exitValue;
+        #endregion
+
+        // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            animator.SetBool(boolName, enterValue);
+        }
+
+        // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateExit is called before OnStateExit is called on any state inside this state machine
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            animator.SetBool(boolName, exitValue);
+        }
+
+        // OnStateMove is called before OnStateMove is called on any state inside this state machine
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateIK is called before OnStateIK is called on any state inside this state machine
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateMachineEnter is called when entering a state machine via its Entry Node
+        //override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+        //{
+        //    
+        //}
+
+        // OnStateMachineExit is called when exiting a state machine via its Exit Node
+        //override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+        //{
+        //    
+        //}
+    }
+}
